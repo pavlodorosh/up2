6e4a601aad2b8275cbb452e0a26057d9ac099ad9
pavlodorosh <dpm1987@gmail.com>
2023-06-26 23:27:28 +0300

    event mouse + keyboard

---
+++ js/event-mouse-keyboard.js
@@ -0,0 +1,34 @@
+document.querySelector('.one').onclick = function (event) {
+    console.log(event);
+    console.log('click');
+};
+
+document.querySelector('.two').onclick = function (event) {
+    console.log('click2');
+};
+
+document.querySelector('.two').ondblclick =function (event) {
+    console.log('double');
+};
+
+document.querySelector('.two').oncontextmenu = function (event) {
+    console.log('right button');
+};
+
+document.querySelector('.i-1').onkeypress = function (event) {
+    console.log('keypress');
+};
+
+document.querySelector('.i-1').onkeydown = function (event) {
+    console.log('keydown');
+
+};
+
+document.querySelector('.i-1').onkeyup = function (event) {
+    console.log('keyup');
+}
+
+document.querySelector('.i-2').onkeypress = function (event) {
+    console.log('keypress');
+
+};
